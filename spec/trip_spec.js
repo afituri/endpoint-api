493ed1b69739644208ace65dbfb968c7ca6c6e0d
Ahmed Fituri <ahmed.elfituri@gmail.com>
2018-04-23 21:06:10 +0300

    Uncomment new trip

--- spec/trip_spec.js
+++ spec/trip_spec.js
@@ -70,7 +70,7 @@ describe('Trip', () => {
       });
   });
 
-  it('creats a new trip', done => {
+  it('creats a new trip by air', done => {
     request.post(
       {
         url: `${apiUrl}/trips`,
@@ -112,6 +112,75 @@ describe('Trip', () => {
         expect(res.statusCode).toBe(201);
         expect(body.trip.travelBy).toBe('air');
         expect(body.trip.service).toBe('package');
+        expect(body.trip.startPoint.location.placeId).toBe('ChIJfVMfhFSIfg0RDyn3Yjf2h0E');
+        expect(body.trip.startPoint.meetingPoint).toBe('some start meeting point');
+        expect(body.trip.startPoint.phone).toBe('092874737282');
+        expect(body.trip.startPoint.location.country).toBe('Algeria');
+        expect(body.trip.startPoint.location.city).toBe('Oran');
+        expect(body.trip.startPoint.location.state).toBe('Oran Province');
+        expect(body.trip.startPoint.location.countryAr).toBe('الجزائر');
+        expect(body.trip.startPoint.location.cityAr).toBe('وهران');
+        expect(body.trip.startPoint.location.stateAr).toBe('ولاية وهران');
+        expect(body.trip.endPoint.location.placeId).toBe('ChIJOwg_06VPwokRYv534QaPC8g');
+        expect(body.trip.endPoint.meetingPoint).toBe('some end meeting point');
+        expect(body.trip.endPoint.phone).toBe('092874737281');
+        expect(body.trip.endPoint.location.country).toBe('United States');
+        expect(body.trip.endPoint.location.city).toBe('New York');
+        expect(body.trip.endPoint.location.state).toBe('New York');
+        expect(body.trip.endPoint.location.countryAr).toBe('الولايات المتحدة');
+        expect(body.trip.endPoint.location.cityAr).toBe('نيويورك');
+        expect(body.trip.endPoint.location.stateAr).toBe('نيويورك');
+        expect(body.trip.travelType).toBe('international');
+        done();
+      }
+    );
+  });
+
+  it('creats a new trip by land', done => {
+    request.post(
+      {
+        url: `${apiUrl}/trips`,
+        headers: {
+          Authorization: `JWT ${token}`
+        },
+        form: {
+          startPoint: {
+            placeId: 'ChIJfVMfhFSIfg0RDyn3Yjf2h0E',
+            meetingPoint: 'some start meeting point',
+            meetingTime: moment()
+              .add(3, 'days')
+              .toJSON(),
+            phone: '092874737282',
+            time: moment()
+              .add(4, 'days')
+              .toJSON()
+          },
+          endPoint: {
+            placeId: 'ChIJOwg_06VPwokRYv534QaPC8g',
+            meetingPoint: 'some end meeting point',
+            meetingTime: moment()
+              .add(6, 'days')
+              .toJSON(),
+            phone: '092874737281',
+            time: moment()
+              .add(5, 'days')
+              .toJSON()
+          },
+          travelBy: 'land',
+          service: 'pool',
+          passengerPrice: 20,
+          vehicleDescription: 'Audi AC, radio',
+          notes: 'some notes about the road'
+        }
+      },
+      (err, res, body) => {
+        body = JSON.parse(body);
+        expect(res.statusCode).toBe(201);
+        expect(body.trip.travelBy).toBe('land');
+        expect(body.trip.service).toBe('pool');
+        expect(body.trip.passengerPrice).toBe(20);
+        expect(body.trip.notes).toBe('some notes about the road');
+        expect(body.trip.vehicleDescription).toBe('Audi AC, radio');
         expect(body.trip.startPoint.location.placeId).toBe('ChIJfVMfhFSIfg0RDyn3Yjf2h0E');
         expect(body.trip.startPoint.meetingPoint).toBe('some start meeting point');
         expect(body.trip.startPoint.phone).toBe('092874737282');
